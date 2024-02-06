4a11dc6bc7bc677daac4365e4395d1d5a7e5614f
David Vega <davidveg@gmail.com>
2024-02-06 11:31:48 -0300

    first commit

---
+++ cmd/catalog/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/devfullcycle/imersao17/goapi/internal/database"
+	"github.com/devfullcycle/imersao17/goapi/internal/service"
+	"github.com/devfullcycle/imersao17/goapi/internal/webserver"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/imersao17")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+
+		}
+	}(db)
+
+	categoryDB := database.NewCategoryDB(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+
+	productDB := database.NewProductDB(db)
+	productService := service.NewProductService(*productDB)
+
+	webCategoryHandler := webserver.NewWebCategoryHandler(categoryService)
+	webProductHandler := webserver.NewWebProductHandler(productService)
+
+	routes := chi.NewRouter()
+	routes.Use(middleware.Logger)
+	routes.Use(middleware.Recoverer)
+	routes.Get("/category/{id}", webCategoryHandler.GetCategory)
+	routes.Get("/category", webCategoryHandler.GetCategories)
+	routes.Post("/category", webCategoryHandler.CreateCategory)
+
+	routes.Get("/product/{id}", webProductHandler.GetProduct)
+	routes.Get("/product", webProductHandler.GetProducts)
+	routes.Get("/product/category/{categoryID}", webProductHandler.GetProductsByCategoryId)
+	routes.Post("/product", webProductHandler.CreateProduct)
+
+	fmt.Println("Server is running on port 8080")
+	err1 := http.ListenAndServe(":8080", routes)
+	if err1 != nil {
+		fmt.Println("ERROR {}", err1)
+		return
+	}
+
+}
---
+++ internal/webserver/category_handler.go
@@ -0,0 +1,64 @@
+package webserver
+
+import (
+	"encoding/json"
+	"github.com/devfullcycle/imersao17/goapi/internal/entity"
+	"github.com/devfullcycle/imersao17/goapi/internal/service"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+type WebCategoryHandler struct {
+	CategoryService *service.CategoryService
+}
+
+func NewWebCategoryHandler(categoryService *service.CategoryService) *WebCategoryHandler {
+	return &WebCategoryHandler{CategoryService: categoryService}
+}
+
+func (wch *WebCategoryHandler) GetCategories(w http.ResponseWriter, r *http.Request) {
+	categories, err := wch.CategoryService.GetCategories()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	err1 := json.NewEncoder(w).Encode(categories)
+	if err1 != nil {
+		return
+	}
+}
+
+func (wch *WebCategoryHandler) GetCategory(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		http.Error(w, "id is required", http.StatusBadRequest)
+		return
+	}
+	category, err := wch.CategoryService.GetCategory(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err1 := json.NewEncoder(w).Encode(category)
+	if err1 != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (wch *WebCategoryHandler) CreateCategory(w http.ResponseWriter, r *http.Request) {
+	var category entity.Category
+	err := json.NewDecoder(r.Body).Decode(&category)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	result, err := wch.CategoryService.CreateCategory(category.Name, category.Description)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err1 := json.NewEncoder(w).Encode(result)
+	if err1 != nil {
+		return
+	}
+}
